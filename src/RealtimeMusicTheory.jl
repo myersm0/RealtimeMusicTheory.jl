dcb5c0802c8ab5473bf86dd3eab1dc7e61680e8c
Michael Myers <michael.myers@wustl.edu>
2025-07-19 08:56:50 -0500

    added exports

--- src/RealtimeMusicTheory.jl
+++ src/RealtimeMusicTheory.jl
@@ -1,5 +1,16 @@
 module RealtimeMusicTheory
 
+export Pitch, C, D, E, F, G, A, B
+export Natural, Sharp, Flat, DoubleSharp, DoubleFlat, ♮, ♯, ♭, ♯♯, ♭♭
+export Interval, Unison, MinorSecond, MajorSecond, MinorThird, MajorThird
+export PerfectFourth, PerfectFifth, MinorSixth, MajorSixth
+export MinorSeventh, MajorSeventh, Octave
+export Scale, degree, MajorScale, NaturalMinorScale
+export Chord, majortriad, minortriad
+export semitone, semitones, offset, next_note, scale_interval, degree
+export majortriad, minortriad, is_major_triad, frequency
+export EqualTemperament
+
 # ===== Core Types =====
 
 abstract type PitchClass end
