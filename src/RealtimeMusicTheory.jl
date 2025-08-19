b6c66353ebd06bf25fe357a401da00bc725c983d
Michael Myers <michael.myers@wustl.edu>
2025-08-18 19:50:14 -0500

    included show.jl

--- src/RealtimeMusicTheory.jl
+++ src/RealtimeMusicTheory.jl
@@ -28,4 +28,6 @@ export IntervalQuality, Perfect, Major, Minor, Augmented, Diminished
 export AbstractInterval, GenericInterval, SpecificInterval, Interval
 export P1, m2, M2, m3, M3, P4, A4, d5, P5, m6, M6, m7, M7, P8
 
+include("show.jl")
+
 end
